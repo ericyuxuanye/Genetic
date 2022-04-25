42f4e5721ce36e11b4989d33eec5921b53cac10e
Eric Ye <yuxuan.smiley@gmail.com>
2022-04-25 16:54:08 -0700

    Added DP Solution to reference against

---
+++ src/com/company/DPApproach.java
@@ -0,0 +1,94 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * Solve using DP, so we get an idea of what the optimal solution should be
+ */
+public class DPApproach {
+    private static final int numCities = 21;
+    private static final int[][] distances = new int[numCities][numCities];
+
+    /**
+     * To store the state for the dynamic programming algorithm
+     */
+    private static final int[][] state = new int[numCities][1<<numCities];
+    /**
+     * To store the previous step for each state
+     */
+    private static final int[][] solution = new int[numCities][1<<numCities];
+
+    public static void main(String[] args) throws IOException {
+        // read in input
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(Objects.requireNonNull(Main.class.getResourceAsStream("/Data.txt"))))) {
+            for (int i = 0; i < numCities; i++) {
+                String line = br.readLine();
+                StringTokenizer st = new StringTokenizer(line);
+                for (int j = 0; j < numCities; j++) {
+                    distances[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+        }
+        // fill state with max value, so that any solution will be better and overwrite it
+        for (int i = 0; i < numCities; i++) {
+            Arrays.fill(state[i], Integer.MAX_VALUE);
+        }
+        // fill solution to -1, so that when program sees -1, we know to stop
+        for (int i = 0; i < numCities; i++) {
+            Arrays.fill(solution[i], -1);
+        }
+        int distance = solve(0, 1);
+        System.out.println("Solution: " + getTour());
+        System.out.println("Distance: " + distance);
+    }
+
+    /**
+     * Solves traveling salesman problem in O(n²2ⁿ) using dynamic programming
+     * <p>
+     * The visited parameter is represented using a bitmask (for speed), starting from the right.
+     * For example, the number 1010 (6 in decimal) means that the second and fourth city are visited
+     * @param pos current city
+     * @param visited for every bit, 1 if visited that city, 0 if not
+     * @return minimal cost for solution
+     */
+    public static int solve(int pos, int visited) {
+        // if we have visited the last city, return distance to first city
+        if (visited == (1<<numCities) - 1) return distances[pos][0];
+        // if we have previously solved for this state, return the result
+        if (state[pos][visited] != Integer.MAX_VALUE) return state[pos][visited];
+
+        int ans = Integer.MAX_VALUE;
+        int bestSolution = 0;
+        for (int i = 0; i < numCities; i++) {
+            // next city cannot be current city, or an already visited city
+            if (i == pos || (visited & (1<<i)) != 0) continue;
+            // try visiting this city next and evaluate cost
+            int distance = distances[pos][i] + solve(i, visited | (1<<i));
+            // if distance is the current best, write that in solution
+            if (distance < ans) {
+                ans = distance;
+                bestSolution = i;
+            }
+        }
+        state[pos][visited] = ans;
+        solution[pos][visited] = bestSolution;
+        return ans;
+    }
+
+    public static String getTour() {
+        String lookup = "XABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder result = new StringBuilder();
+        int curr = 0;
+        int visited = 1;
+        do {
+            result.append(lookup.charAt(curr));
+            curr = solution[curr][visited];
+            visited = visited | (1 << curr);
+        } while (curr != -1);
+        result.append('X');
+        return result.toString();
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,14 @@
 package com.company;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+
 public class Main {
     static int numCities = 21;
     static int matingPoolSize = 4;
+    static final Random rand = new Random();
     public static void main(String[] args) throws IOException {
         int[][] distance = new int[numCities][numCities];
         try (BufferedReader br = new BufferedReader(new InputStreamReader(Objects.requireNonNull(Main.class.getResourceAsStream("/Data.txt"))))) {
@@ -17,83 +20,86 @@ public class Main {
                 }
             }
         }
-        populateRandom(distance);
-        System.out.println("Random distances: " +
-                Arrays.deepToString(distance));
-//method to pick random tour
+        //method to pick random tour
         int[] randomTour = randomTour(numCities);
-        System.out.println("Random Tour: " + Arrays.toString(randomTour));
-//method to evaluate fitness of a tour
+        System.out.println("Random Tour: " + getTour(randomTour));
+        //method to evaluate fitness of a tour
         System.out.println("Tour Fitness: " + tourFitness(distance,
                 randomTour));
-//Pick random tours to make up mating pool
+        //Pick random tours to make up mating pool
         int[][] matingPool = KrandomTours(numCities, matingPoolSize);
     }
-    public static void populateRandom(int[][] arr){
+
+    public static void populateRandom(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
-                arr[i][j] = (int) (Math.random()*10);
+                arr[i][j] = (int) (Math.random() * 10);
             }
         }
     }
-    public static int[] randomTour(int numCities){
-        ArrayList<Integer> randomTourInList = randomTour(numCities, new
-                ArrayList<>());
-        int[] ret = new int[numCities];
+
+    public static int[] randomTour(int numCities) {
+        int[] result = new int[numCities];
+        // fill with random numbers
         for (int i = 0; i < numCities; i++) {
-            ret[i] = randomTourInList.get(i);
+            result[i] = i;
         }
-        return ret;
+        // shuffle using fisher-yates
+        for (int i = numCities - 1; i > 0; i--) {
+            // random number from 0 (inclusive) to i (exclusive)
+            int randIndex = rand.nextInt(i);
+            // swap current with item at randIndex
+            int temp = result[i];
+            result[i] = result[randIndex];
+            result[randIndex] = temp;
+        }
+        return result;
     }
-    public static ArrayList<Integer> randomTour(int numCities,
-                                                ArrayList<Integer> currTour){
-        ArrayList<Integer> citiesNotInTour = new ArrayList<>();
-        for (int i = 0; i < numCities; i++) {
-            if(!currTour.contains(i)) citiesNotInTour.add(i);
-        }
-        if(citiesNotInTour.size()==0){
-            return currTour;
-        }
-        else{
-            int randIndex = (int)
-                    (Math.random()*(citiesNotInTour.size()));
-            int cityToAddToTour = citiesNotInTour.get(randIndex);
-            currTour.add(cityToAddToTour);
-            randomTour(numCities, currTour);
-        }
-        return currTour; //never reached
-    }
-    public static int tourFitness(int[][] distance, int[] tour){
+
+    public static int tourFitness(int[][] distance, int[] tour) {
         int fitness = 0;
-        for (int i = 0; i < tour.length-1; i++) {
+        for (int i = 0; i < tour.length - 1; i++) {
             int cityA = tour[i];
-            int cityB = tour[i+1];
+            int cityB = tour[i + 1];
             fitness += distance[cityA][cityB];
         }
+        // add distance from last city to first city
+        fitness += distance[tour[tour.length - 1]][tour[0]];
         return fitness;
     }
-    public static int[][] KrandomTours(int numCities, int K){
+
+    public static int[][] KrandomTours(int numCities, int K) {
         int[][] ret = new int[K][numCities];
         HashSet<int[]> setOfRandomTours = new HashSet<>();
-        while (setOfRandomTours.size() < K){
+        while (setOfRandomTours.size() < K) {
             int[] newRandomTour = randomTour(numCities);
-//check if newRandomTour is in setOfRandomTours
+            //check if newRandomTour is in setOfRandomTours
             boolean newRandomTourInSet = false;
-            for(int[] randomTour : setOfRandomTours){
-                if(Arrays.equals(newRandomTour, randomTour))
-                    newRandomTourInSet=true;
+            for (int[] randomTour : setOfRandomTours) {
+                if (Arrays.equals(newRandomTour, randomTour)) {
+                    newRandomTourInSet = true;
+                    break;
+                }
             }
-//if newRandomTour not in setOfRandomTours, add it
-            if(newRandomTourInSet == false)
+            //if newRandomTour not in setOfRandomTours, add it
+            if (!newRandomTourInSet)
                 setOfRandomTours.add(newRandomTour);
             System.out.println();
         }
-//transfer set to array
-        int ind=0;
-        for(int[] tour : setOfRandomTours){
+        //transfer set to array
+        int ind = 0;
+        for (int[] tour : setOfRandomTours) {
             ret[ind] = tour;
             ind++;
         }
         return ret;
+    }
+    public static String getTour(int[] tour) {
+        String lookup = "XABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder result = new StringBuilder();
+        for (int i : tour) {
+            result.append(lookup.charAt(i));
+        }
+        return result.toString();
     }
 }
